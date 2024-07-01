4bf706c47c44d0bc3cafdd69ba3270036e9b3584
Pcocyo <joshuaelson46@example.com>
2024-07-01 15:29:56 +0800

    changing socket io ports 2

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import io from 'socket.io-client'
 
-const socket = io('https://chatapp-backend-yd3m.onrender.com/')
+const socket = io('https://chatapp-backend-yd3m.onrender.com')
 
 const useSocket = (eventName, callback) => { //custom event to handle socket event
     useEffect(() => {
